074eb42ae16eced2769247070e4fc20ff15ca62a
abdullahsohaill <26100142@lums.edu.pk>
2026-01-25 18:55:44 +0500

    lab1 done

---
+++ Assignment1/LonelyTwitter/app/src/main/java/com/example/lonelytwitter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.lonelytwitter;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ImportantTweet t1 = new ImportantTweet("this is an important tweet");
+        NormalTweet t2 = new NormalTweet("this is just a normal tweet");
+
+        ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+
+        tweetList.add(t1);
+        tweetList.add(t2);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
